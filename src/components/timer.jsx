a33d2ce7b126e7b39768af501ab5e2e8de936243
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-02-23 20:55:23 +0800

    Timer only

---
+++ src/components/timer.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
+import "../output.css";
+
+function Timer() {
+  const currentTime = useRef(0);
+  const [time, setTime] = useState(0);
+  const [interval, assignInterval] = useState(null);
+  let second = 0,
+    elapsedTime = null;
+
+  useEffect(() => {
+    const minute = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    currentTime.current.innerHTML = `${pad(String(minute))} : ${pad(
+      String(seconds)
+    )}`;
+  }, [time]);
+
+  const pad = (value) => {
+    return value.padStart(2, "0");
+  };
+
+  const startTimer = () => {
+    if (interval) {
+      pauseTimer();
+    }
+    assignInterval(setInterval(incrementNumber, 1000));
+  };
+
+  const pauseTimer = () => {
+    clearInterval(interval);
+  };
+
+  const incrementNumber = () => {
+    second++;
+    setTime(second);
+  };
+
+  const resetTimer = () => {
+    clearInterval(interval);
+    currentTime.current.innerHTML = "00 : 00";
+    setTime(0);
+  };
+
+  return (
+    <div className="flex items-center flex-col mt-[130px]  gap-[40px]">
+      <h2
+        className="text-black text-center p-5 text-[100px] font-['Jetbrains_Mono']"
+        ref={currentTime}
+      >
+        {time};
+      </h2>
+      <div className="flex gap-10">
+        <button
+          className="border-2 border-black w-[130px] p-2 rounded-[400px] font-[Jetbrains_Mono] hover:bg-black hover:text-white"
+          onClick={startTimer}
+        >
+          Start
+        </button>
+        <button
+          className="border-2 border-black w-[130px] p-2 rounded-[400px] font-[Jetbrains_Mono]  hover:bg-black hover:text-white"
+          onClick={pauseTimer}
+        >
+          Pause
+        </button>
+        <button
+          className="border-2 border-black w-[130px] p-2 rounded-[400px] font-[Jetbrains_Mono]  hover:bg-black hover:text-white"
+          onClick={resetTimer}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
